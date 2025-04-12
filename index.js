7e628d382b2a3d7ae9e03c01f5d86d6631ce84ba
TysonPWilliams <tyson.williams95@gmail.com>
2025-04-12 13:28:59 +1000

    Started Posts routes

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-const express = require('express')
+const express = require('express') // CommonJS modules
 const app = express()
-const port = 3000
+const port = 8080
 
+const person = {
+  name: "Matt",
+  age: 52
+}
+
+const posts = [
+  {
+    title: 'Post 1',
+    body: 'Lorem ipsum dolor sit amet',
+    isPublished: false
+  },
+  {
+    title: 'Post 2',
+    body: 'This is the body of post 2',
+    isPublished: true
+  }
+]
+
+console.clear()
+console.log('Server restarting...', new Date())
 // Python:
 // @app.route('/', methods=['GET'])  // default get request
 // def home():
@@ -10,9 +30,20 @@ const port = 3000
 // app.[http-verb]([path], [callback])
 // Callback accepts a request and response object
 app.get('/', (req, res) => {
-  res.send('Hello Here!')
+  // Serialize person and send to the client
+  res.send(person)
 })
 
+// Posts routes
+
+// Get all posts
+app.get('/posts', (req, res) => {
+  res.send(posts)
+})
+// Get one post
+// Create a post
+// Update a post
+// Delete a post
 
 // Start the dev server on the given port
 // The callback is called when the server is running
