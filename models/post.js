7f02774fc1531c3ea60bb5073afb47dbc51d31ff
TysonPWilliams <tyson.williams95@gmail.com>
2025-04-28 19:10:30 +1000

    Small change to comments

--- models/post.js
+++ models/post.js
@@ -1,6 +1,6 @@
 import { model } from 'mongoose'
 
-// Flask: ORM (Object-Relational Mapper - SQLAlchemy)
+// Flask: ORM (Object-Relational Mapper - SQLAlchemy) - defined models
 // Express: ODM (Object-Document Mapper - Mongoose)
 const Post = model('Post', {
     title: { type: String, required: true },
